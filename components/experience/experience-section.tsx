f55d9e6f8f78d19da3c57cf3a6d89408e40d6e29
Julio <julioarhernandez@gmail.com>
2026-08-24 21:27:01 -0400

    Expereicne

--- components/experience/experience-section.tsx
+++ components/experience/experience-section.tsx
@@ -13,9 +13,12 @@ export function ExperienceSection() {
           <span aria-hidden="true" className="mt-3 block h-1 w-10 bg-primary" />
         </div>
       </Reveal>
-      <div className="relative sm:ml-2">
-        <div aria-hidden="true" className="absolute top-2 bottom-2 left-4 w-px bg-border" />
-        <Reveal stagger className="space-y-8 sm:space-y-9">
+      <div className="relative">
+        <div
+          aria-hidden="true"
+          className="absolute top-2 bottom-2 left-4 w-px bg-border sm:left-[10.5rem]"
+        />
+        <Reveal stagger className="space-y-8 sm:space-y-6">
           {experience.map((item) => (
             <ExperienceCard key={item.company} item={item} />
           ))}
